219eff5cf08058abc08a222913e28d6df772fdbe
Abhishek Srivastava <abhishek77in@gmail.com>
2013-09-09 20:15:55 +0530

    Configure paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ PaperclipExample::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # configure paperclip
+  Paperclip.options[:command_path] = "/usr/bin/"
 end
